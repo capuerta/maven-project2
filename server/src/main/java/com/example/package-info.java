11a7bf261a65bdd9a3a9bae22e6c09f4c822a9ae
capuerta <capuertal@hotmail.com>
2019-03-14 15:54:41 +0000

    Update package-info.java

--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/******************************
  *  TODO1 SERVICES, INC. ('TODO1') All rights reserved, 2000, 2019.
  * This work is protected by the United States of America copyright laws.
  * All information contained herein is and remains the property of
@@ -6,7 +6,7 @@
  * Dissemination of this information or reproduction of this material
  * is not permitted unless prior written consent is obtained from
  * TODO1 SERVICES, INC.
- *******************************************************************************/
+ *******************************/
 /**
  * @author cpuerta
  */
