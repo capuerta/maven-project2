1692be585e8ae5afc6039ac082dfef07cc957fb6
capuerta <capuertal@hotmail.com>
2019-03-14 10:19:21 -0500

    Agregado package-info

---
+++ server/src/main/java/com/example/package-info.java
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * © TODO1 SERVICES, INC. ('TODO1') All rights reserved, 2000, 2019.
+ * 
+ * This work is protected by the United States of America copyright laws.
+ * All information contained herein is and remains the property of
+ * TODO1 [and its suppliers, if any].
+ * Dissemination of this information or reproduction of this material
+ * is not permitted unless prior written consent is obtained from
+ * TODO1 SERVICES, INC.
+ *******************************************************************************/
+/**
+ * @author cpuerta
+ *
+ */
+package com.example;
